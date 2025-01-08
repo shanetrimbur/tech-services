082b3f29b6c7460e07eb7b043f5522c4567c2255
shanetrimbur <shanetrimbur@gmail.com>
2025-01-07 18:05:16 -0800

    updates

--- src/navigation.ts
+++ src/navigation.ts
@@ -178,22 +178,22 @@ export const footerData = {
       //],
     },
     {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
+      //title: 'Company',
+      //links: [
+      //  { text: 'About', href: '#' },
+      //  { text: 'Blog', href: '#' },
+      //  { text: 'Careers', href: '#' },
+      //  { text: 'Press', href: '#' },
+      //  { text: 'Inclusion', href: '#' },
+      //  { text: 'Social Impact', href: '#' },
+      //  { text: 'Shop', href: '#' },
+      //],
     },
   ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
+  //secondaryLinks: [
+  //  { text: 'Terms', href: getPermalink('/terms') },
+  //  { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  //],
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
@@ -203,6 +203,6 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="~/assets/favicons/favicon.ico" alt=" " loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://defensetechsolutions.com/"> Defense Technical Solutions</a> · All rights reserved.
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://trimburtech.com"> Trimbur Tech</a> · All rights reserved.
   `,
 };
