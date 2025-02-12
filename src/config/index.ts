b135e430ce5e63d3b8719bd8693af28061dc8f69
shanetrimbur <shanetrimbur@gmail.com>
2025-02-11 21:37:32 -0800

    Configure GitHub Pages deployment

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,20 +1,16 @@
+import yaml from 'js-yaml';
+import fs from 'node:fs';
+import path from 'node:path';
+
+const configPath = path.resolve('./src/config.yaml');
+const configYaml = yaml.load(fs.readFileSync(configPath, 'utf8')) as Record<string, any>;
+
 export const SITE = {
-  name: 'TrimburTech',
-  site: 'https://trimburtech.com',
-  base: '/',
-  trailingSlash: false,
-  googleSiteVerificationId: ''
+  name: configYaml.site.name,
+  site: configYaml.site.site,
+  base: configYaml.site.base,
+  trailingSlash: configYaml.site.trailingSlash,
+  googleSiteVerificationId: configYaml.site.googleSiteVerificationId
 };
 
-export const APP_BLOG = {
-  isEnabled: true,
-  postsPerPage: 6,
-  post: {
-    isEnabled: true,
-    permalink: '/%slug%',
-    robots: {
-      index: true,
-      follow: true,
-    },
-  },
-}; 
+export const APP_BLOG = configYaml.apps.blog; 
